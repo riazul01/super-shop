5d62cc9e9e3b019ed2ba3cd8c30b98febcebb8c0
Md Riazul Islam <riazul5207@gmail.com>
2023-08-18 21:27:28 +0600

    products ui completed💥

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('Hello World!');
+const products = [
+    {
+        id: 101,
+        category: 'vegetables',
+        name: 'Mango',
+        price: 120,
+        currency: 'Tk',
+        unit: 'Kg',
+        discountInPercent: 20,
+        addedInCart: 0,
+        addedToFavorite: false,
+        cartAddedTime: '',
+        imageUrl: './images/vegetables/mango.jpg'
+    }
+];
