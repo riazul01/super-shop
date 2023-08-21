f4e6a986c1d513d1006e3fb82cb9e73e42e8ba90
Md Riazul Islam <riazul5207@gmail.com>
2023-08-21 21:31:27 +0600

    hero section completed💥

--- script.js
+++ script.js
@@ -1,20 +1,130 @@
 import { products } from "./data.js";
 
+// header
 let header = document.querySelector('header');
-let sectionWrap = document.querySelector('.section-wrap');
+let sections = document.querySelector('.sections');
 
 window.onscroll = () => {
     let scrollTop = document.documentElement.scrollTop;
 
     if (scrollTop > header.offsetHeight) {
         header.classList.add('active-header');
-        sectionWrap.style.marginTop = header.offsetHeight + 'px';
+        sections.style.marginTop = header.offsetHeight + 'px';
     } else {
         header.classList.remove('active-header');
-        sectionWrap.style.marginTop = 0 + 'px';
+        sections.style.marginTop = 0 + 'px';
     }
 }
 
+
+
+
+
+// hero
+let heroImg = document.querySelector('.hero-image');
+let heroDescription = document.querySelector('.hero-description');
+
+// update hero image
+function updateHeroImage(imgIndex) {
+    let imgSrc = ['01.png', '02.png', '03.png', '04.png', '05.png', '06.png'];
+    let oldImg = document.querySelector('.hero-image img');
+    heroImg.removeChild(oldImg);
+
+    let newImg = document.createElement('img');
+    newImg.setAttribute('class', 'animate__animated animate__bounceInDown');
+    newImg.src = `./images/hero/${imgSrc[imgIndex]}`;
+
+    heroImg.appendChild(newImg);
+}
+
+// update hero title
+function updateHeroTitle(titleIndex) {
+    let txt1 = `The best dried fruits for your family health`;
+    let txt2 = `Get your daily needs easy and instant`;
+    let txt3 = `Try fresh fruits for better healthy lifestyle`;
+    let txt4 = `Get upto 50% discount on every products`;
+    let txt5 = `Hot deals available with amazing products`;
+    let txt6 = `Fresh vegetables with a big discount`;
+
+    let textAra = [txt1, txt2, txt3, txt4, txt5, txt6];
+
+    let oldText = document.querySelector('.hero-title');
+    heroDescription.removeChild(oldText);
+
+    let newTitle = document.createElement('h1');
+    newTitle.setAttribute('class', 'animate__animated animate__bounceInRight hero-title');
+    newTitle.textContent = `${textAra[titleIndex]}`;
+
+    heroDescription.appendChild(newTitle);
+}
+
+// update hero button
+function updateHeroButton() {
+    let oldButton = document.querySelector('.hero-button');
+    heroDescription.removeChild(oldButton);
+
+    let newButton = document.createElement('button');
+    newButton.textContent = `Explore`;
+    newButton.setAttribute('class', 'animate__animated animate__bounceInUp hero-button');
+
+    heroDescription.appendChild(newButton);
+}
+
+// initiate slide position
+let heroIndex = 0;
+
+// slide right
+function heroSlideRight() {
+    heroIndex++;
+
+    if (heroIndex === 6) {
+        heroIndex = heroIndex % 6;
+    }
+
+    updateHeroImage(heroIndex);
+    updateHeroTitle(heroIndex);
+    updateHeroButton();
+}
+
+// slide left
+function heroSlideLeft() {
+    heroIndex--;
+
+    if (heroIndex < 0) {
+        heroIndex = 5;
+    }
+
+    updateHeroImage(heroIndex);
+    updateHeroTitle(heroIndex);
+    updateHeroButton();
+}
+
+// auto slide
+let heroSlide = setInterval(() => {
+
+    heroSlideRight();
+
+}, 10000);
+
+// slider button
+let heroSlideLeftBtn = document.querySelector('.hero-slide-left');
+let heroSlideRightBtn = document.querySelector('.hero-slide-right');
+
+// actions while click left arrow button
+heroSlideLeftBtn.onclick = () => {
+    heroSlideLeft();
+}
+
+// actions while click right arrow button
+heroSlideRightBtn.onclick = () => {
+    heroSlideRight();
+}
+
+
+
+
+
+// products
 const createProductCard = (product) => {
     const productCard = document.createElement('div');
     productCard.setAttribute('class', 'product-card');
