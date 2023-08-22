709fd3b09ce0782ba20177e832392724bc1a978c
Md Riazul Islam <riazul5207@gmail.com>
2023-08-22 10:43:31 +0600

    call to action section completed💥

--- script.js
+++ script.js
@@ -124,6 +124,38 @@ heroSlideRightBtn.onclick = () => {
 
 
 
+
+
+
+
+
+// Call to action
+let arrivalCard = document.querySelectorAll('.new-arrival-card');
+
+// three cards width including each of their margin right
+let move = (arrivalCard[0].offsetWidth + 20) * 3;
+
+// slide three cards in every 3 seconds
+let newArrival = setInterval(() => {
+    if (move >= ((arrivalCard[0].offsetWidth + 20) * (arrivalCard.length / 2))) {
+        move = 0;
+    }
+
+    for (let i = 0; i < arrivalCard.length; i++) {
+        arrivalCard[i].style.transform = `translateX(-${move}px)`;
+    }
+
+    move += (arrivalCard[0].offsetWidth + 20) * 3;
+
+}, 3000);
+
+
+
+
+
+
+
+
 // products
 const createProductCard = (product) => {
     const productCard = document.createElement('div');
