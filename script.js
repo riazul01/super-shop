61a270f8c318e4272aa4ba24e7862dc031f53ffc
Riazul Islam <riazul5207@gmail.com>
2023-11-02 23:40:16 +0600

    hero section updated🚀

--- script.js
+++ script.js
@@ -22,105 +22,113 @@ import { products } from "./data.js";
 
 
 
-// hero
-let heroImg = document.querySelector('.hero-image');
-let heroDescription = document.querySelector('.hero-description');
+// HERO
+(function() {
+    let heroImg = document.querySelector('.hero-image');
+    let heroDesc = document.querySelector('.hero-desc');
 
-// update hero image
-function updateHeroImage(imgIndex) {
-    let imgSrc = ['01.png', '02.png', '03.png', '04.png', '05.png', '06.png'];
-    let oldImg = document.querySelector('.hero-image img');
-    heroImg.removeChild(oldImg);
+    // update hero image
+    const updateHeroImage = (imgIndex) => {
+        let imgSrc = ['01.png', '02.png', '03.png', '04.png', '05.png', '06.png'];
+        let oldImg = document.querySelector('.hero-image img');
+        heroImg.removeChild(oldImg);
 
-    let newImg = document.createElement('img');
-    newImg.setAttribute('class', 'animate__animated animate__bounceInDown');
-    newImg.src = `./images/hero/${imgSrc[imgIndex]}`;
+        let newImg = document.createElement('img');
+        newImg.setAttribute('class', 'animate__animated animate__bounceInDown');
+        newImg.src = `../images/hero/${imgSrc[imgIndex]}`;
 
-    heroImg.appendChild(newImg);
-}
-
-// update hero title
-function updateHeroTitle(titleIndex) {
-    let txt1 = `The best dried fruits for your family health`;
-    let txt2 = `Get your daily needs easy and instant`;
-    let txt3 = `Try fresh fruits for better healthy lifestyle`;
-    let txt4 = `Get upto 50% discount on every products`;
-    let txt5 = `Hot deals available with amazing products`;
-    let txt6 = `Fresh vegetables with a big discount`;
-
-    let textAra = [txt1, txt2, txt3, txt4, txt5, txt6];
-
-    let oldText = document.querySelector('.hero-title');
-    heroDescription.removeChild(oldText);
-
-    let newTitle = document.createElement('h1');
-    newTitle.setAttribute('class', 'animate__animated animate__bounceInRight hero-title');
-    newTitle.textContent = `${textAra[titleIndex]}`;
-
-    heroDescription.appendChild(newTitle);
-}
-
-// update hero button
-function updateHeroButton() {
-    let oldButton = document.querySelector('.hero-button');
-    heroDescription.removeChild(oldButton);
-
-    let newButton = document.createElement('button');
-    newButton.textContent = `Explore`;
-    newButton.setAttribute('class', 'animate__animated animate__bounceInUp hero-button');
-
-    heroDescription.appendChild(newButton);
-}
-
-// initiate slide position
-let heroIndex = 0;
-
-// slide right
-function heroSlideRight() {
-    heroIndex++;
-
-    if (heroIndex === 6) {
-        heroIndex = heroIndex % 6;
+        heroImg.appendChild(newImg);
     }
 
-    updateHeroImage(heroIndex);
-    updateHeroTitle(heroIndex);
-    updateHeroButton();
-}
+    // update hero title
+    const updateHeroTitle = (titleIndex) => {
+        let txt1 = `The best dried fruits for your family health`;
+        let txt2 = `Get your daily needs easy and instant`;
+        let txt3 = `Try fresh fruits for better healthy lifestyle`;
+        let txt4 = `Get upto 50% discount on every products`;
+        let txt5 = `Hot deals available with amazing products`;
+        let txt6 = `Fresh vegetables with a big discount`;
 
-// slide left
-function heroSlideLeft() {
-    heroIndex--;
+        let textAra = [txt1, txt2, txt3, txt4, txt5, txt6];
 
-    if (heroIndex < 0) {
-        heroIndex = 5;
+        let oldText = document.querySelector('.hero-title');
+        heroDesc.removeChild(oldText);
+
+        let newTitle = document.createElement('h1');
+        newTitle.setAttribute('class', 'animate__animated animate__bounceInRight hero-title');
+        newTitle.textContent = `${textAra[titleIndex]}`;
+
+        heroDesc.appendChild(newTitle);
     }
 
-    updateHeroImage(heroIndex);
-    updateHeroTitle(heroIndex);
-    updateHeroButton();
-}
+    // update hero button
+    const updateHeroButton = () => {
+        let oldButton = document.querySelector('.hero-button');
+        heroDesc.removeChild(oldButton);
 
-// auto slide
-let heroSlide = setInterval(() => {
+        let newButton = document.createElement('button');
+        newButton.textContent = `Explore`;
+        newButton.setAttribute('class', 'animate__animated animate__bounceInUp hero-button');
 
-    heroSlideRight();
+        heroDesc.appendChild(newButton);
+    }
 
-}, 10000);
+    // initial slide position
+    let heroIndex = 0;
 
-// slider button
-let heroSlideLeftBtn = document.querySelector('.hero-slide-left');
-let heroSlideRightBtn = document.querySelector('.hero-slide-right');
+    // slide right
+    const heroSlideRight = () => {
+        heroIndex++;
 
-// actions while click left arrow button
-heroSlideLeftBtn.onclick = () => {
-    heroSlideLeft();
-}
+        if (heroIndex === 6) {
+            heroIndex = heroIndex % 6;
+        }
 
-// actions while click right arrow button
-heroSlideRightBtn.onclick = () => {
-    heroSlideRight();
-}
+        updateHeroImage(heroIndex);
+        updateHeroTitle(heroIndex);
+        updateHeroButton();
+    }
+
+    // slide left
+    const heroSlideLeft = () => {
+        heroIndex--;
+
+        if (heroIndex < 0) {
+            heroIndex = 5;
+        }
+
+        updateHeroImage(heroIndex);
+        updateHeroTitle(heroIndex);
+        updateHeroButton();
+    }
+
+    let slideTimerId = null;
+
+    // auto slide
+    const autoSlideRight = () => {
+        slideTimerId = setInterval(() => {
+            heroSlideRight();
+        }, 10000);
+    }
+
+    // slider button
+    let heroSlideLeftBtn = document.querySelector('.hero-slide-left');
+    let heroSlideRightBtn = document.querySelector('.hero-slide-right');
+
+    heroSlideLeftBtn.onclick = () => {
+        clearInterval(slideTimerId);
+        heroSlideLeft();
+        autoSlideRight();
+    }
+
+    heroSlideRightBtn.onclick = () => {
+        clearInterval(slideTimerId);
+        heroSlideRight();
+        autoSlideRight();
+    }
+
+    autoSlideRight();
+})();
 
 
 
