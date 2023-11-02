8fc784d8eb69605f338b90e32d47a0005f4722eb
Riazul Islam <riazul5207@gmail.com>
2023-11-02 23:28:25 +0600

    header section updated✈️

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 import { products } from "./data.js";
 
-// header
-let header = document.querySelector('header');
-let sections = document.querySelector('.sections');
+// HEADER
+(function() {
+    let header = document.querySelector('header');
+    let sections = document.querySelector('.sections');
 
-window.onscroll = () => {
-    let scrollTop = document.documentElement.scrollTop;
+    window.onscroll = () => {
+        let scrollTop = document.documentElement.scrollTop;
 
-    if (scrollTop > header.offsetHeight) {
-        header.classList.add('active-header');
-        sections.style.marginTop = header.offsetHeight + 'px';
-    } else {
-        header.classList.remove('active-header');
-        sections.style.marginTop = 0 + 'px';
+        if (scrollTop > header.offsetHeight) {
+            header.classList.add('active-header');
+            sections.style.marginTop = header.offsetHeight + 'px';
+        } else {
+            header.classList.remove('active-header');
+            sections.style.marginTop = 0 + 'px';
+        }
     }
-}
+})();
 
 
 
