593e5073286b7acd30273b5f820c0e015d56c07e
Riazul Islam <riazul5207@gmail.com>
2023-11-04 01:30:20 +0600

    faq section completed💥

--- js/script.js
+++ js/script.js
@@ -15,3 +15,47 @@
         }
     }
 })();
+
+
+
+// FAQ
+(function() {
+    let faqHeader = document.querySelectorAll('.faq-header');
+    let faqText = document.querySelectorAll('.faq-text');
+    let faqIcon = document.querySelectorAll('.faq-icon');
+
+    let accordion = document.querySelector('.faq-accordion');
+    let body = document.querySelector('body');
+
+    let prevIndex = null;
+
+    for (let i = 0; i < faqHeader.length; i++) {
+        faqHeader[i].addEventListener('click', () => {
+            for (let j = 0; j < faqText.length; j++) {
+                faqText[j].classList.remove('active-faq-text');
+                faqIcon[j].classList.remove('active-faq-icon');
+            }
+
+            if (prevIndex === i) {
+                prevIndex = null;
+            } else {
+                faqText[i].classList.add('active-faq-text');
+                faqIcon[i].classList.add('active-faq-icon');
+                prevIndex = i;
+            }
+        });
+    }
+
+    // close accrodion box
+    body.onclick = () => {
+        for (let i = 0; i < faqHeader.length; i ++) {
+            faqText[i].classList.remove('active-faq-text');
+            faqIcon[i].classList.remove('active-faq-icon');
+            prevIndex = null;
+        }
+    }
+
+    accordion.onclick = (e) => {
+        e.stopPropagation();
+    }
+})();
