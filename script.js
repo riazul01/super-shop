6e65febbfeba8025ea642319ac2c2bffc3a323a4
Riazul Islam <riazul5207@gmail.com>
2023-11-03 15:19:11 +0600

    discount section functionality added💨

--- script.js
+++ script.js
@@ -371,3 +371,75 @@ if (document.querySelector('.home')) {
     renderProductsBySubcategory();
     renderProductsByCategory();
 }
+
+
+
+// GREAT DISCOUNT
+(function() {
+    let greatDiscountWrap = document.querySelector('.great-discount-wrap');
+    let image = document.querySelector('.gd-image > img');
+    let savePer = document.querySelector('.save-per');
+    let productName = document.querySelector('.gd-product-name');
+    let productPrice = document.querySelector('.gd-product-price');
+    let discountMessage = document.querySelector('.gd-discount-message');
+
+    let product = products.find((item) => item.id.toString() === greatDiscountWrap.getAttribute('data-id'));
+
+    // initialize date, month, year
+    let date = new Date();
+    let monthIndex = date.getMonth();
+    let year = date.getFullYear();
+
+    const createInfiniteCountdown = (date, time) => {
+        let curntTime = Date.now();
+        let month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+        let eventTime = new Date(`${month[monthIndex]} ${date}, ${year} ${time}`).getTime();
+        let totalSeconds = (eventTime - curntTime) / 1000;
+
+        if (totalSeconds < 0) {
+            monthIndex = (monthIndex + 1) % 12;
+
+            if (monthIndex === 0) {
+                year = year + 1;
+            }
+
+            let eventTime = new Date(`${month[monthIndex]} ${date}, ${year} ${time}`).getTime();
+            totalSeconds = (eventTime - curntTime) / 1000;
+        }
+
+        let dayConst = 86400;
+        let hourConst = 3600;
+        let minuteConst = 60;
+
+        let days = Math.floor(totalSeconds / dayConst);
+        totalSeconds = totalSeconds % dayConst;
+
+        let hours = Math.floor(totalSeconds / hourConst);
+        totalSeconds = totalSeconds % hourConst;
+
+        let minutes = Math.floor(totalSeconds / minuteConst);
+        totalSeconds = totalSeconds % minuteConst;
+
+        let seconds = Math.floor(totalSeconds);
+
+        days = (days < 10) ? '0' + days : days;
+        hours = (hours < 10) ? '0' + hours : hours;
+        minutes = (minutes < 10) ? '0' + minutes : minutes;
+        seconds = (seconds < 10) ? '0' + seconds : seconds;
+
+        return [days, hours, minutes, seconds];
+    }
+
+    image.src = product.imageUrl;
+    image.alt = product.name.split(' ').join('-');
+    savePer.textContent = `Save upto ${product.discountInPercent}%`;
+    productName.textContent = (product.name.split(' ')[0].toLowerCase() === 'fresh') ? product.name : `Fresh ${product.name}`;
+    productPrice.textContent = `${product.price - (product.price * (product.discountInPercent / 100))}${product.currency} Only`;
+    
+    // infinite countdown
+    let timeCount = setInterval(() => {
+        let times = createInfiniteCountdown('28', '12:00:00');
+        discountMessage.textContent =  `Offer ends in ${times[0]}d ${times[1]}h ${times[2]}m ${times[3]}s`;
+    }, 1000);
+})();
