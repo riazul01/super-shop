efc36879cdda5eaeadaaf8f6dcab3f055cab3e3a
Riazul Islam <riazul5207@gmail.com>
2023-11-21 18:39:00 +0600

    products section ui updated🚀

--- js/products.js
+++ js/products.js
@@ -193,6 +193,5 @@ const renderProductsByCategory = () => {
 if (document.querySelector('#home')) {
     renderProductsBySubcategory();
 } else if (document.querySelector('#products')) {
-    renderProductsBySubcategory();
     renderProductsByCategory();
 }
